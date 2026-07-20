c6b67b55b46dcd93e6fc9c29bc74b0d5a6e270aa
Pushpa Kumari Nandam <nandampushpakumari526@gmail.com>
2026-07-20 11:00:07 +0530

    Add files via upload

---
+++ programs.py
@@ -0,0 +1,124 @@
+'''#voting
+a=int(input())
+if a>=18:
+    print("eligible for voting")
+else:
+    print("not eligible")
+#even or odd
+a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")
+#leap year
+year=int(input())
+if year% 4==0 and year%100!=0:
+    print("leap")
+else:
+    print("not leap")s
+#guest code
+a=input().lower()
+if a=="pushpa":
+    print("WELCOME")
+else:
+    print("WELCOME GUEST")
+a=["pushpa","kumari","teja","devi","bharani"]
+b=input()
+if b in a:
+    print(b,"welcome")
+else:
+    print("welcome guest")'''
+'''a=["a","e","i","o","u"]
+b=input().lower()
+if b in a:
+    print("vowel")
+else:
+    print("consonant")'''
+
+
+
+#social media login
+'''name=input("Enter user name")
+password=int(input("Enter you are password: "))
+if name=="pushpa":
+    if password==1234:
+        print("Successfully login")
+else:
+    print("Invalid password")
+
+name=input("Enter user name")
+password=int(input("Enter you are password: "))
+if name=="pushpa":
+    if password==1234:
+        print("Successfully login")
+    else:
+        print("incorrect password")
+else:
+    print("Invalid password")
+    
+name=input("Enter user name")
+password=input("Enter you are password: ")
+if name=="pushpa" and password=="pushpa@1234":
+        print("Successfully login")
+else:
+    print("Invalid password")
+age=int(input())
+marks=int(input())
+atte=int(input())
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible")
+if marks>=70:
+    print("allow to write exam")
+if atte>=80:
+    print("they are eligible for scholarshop")
+
+#Bakery cakes
+
+cake=input("choose u r cake name").lower()
+if cake=="red velvet":
+    print("{} cost is 1200 rupees".format(cake))
+elif cake=="choco almond":
+    print("{} cost is 1000 rupees".format(cake))
+elif cake=="honey almond":
+    print("{} cost is 800 rupees".format(cake))
+elif cake=="butterscotch":
+    print("{} cost is 600 rupees".format(cake))
+else:
+    print("{} is not avaliable".format(cake))'''
+
+
+
+#pizza shipps
+
+price=int(input("choose the price"))
+if price==1000:
+    print("BBQ pizza")
+elif price==800:
+    print("crispy chicken piza")
+elif price==600:
+    print("panner pizza")
+elif price==400:
+    print("corn pizza")
+elif price==200:
+    print("frenchfrics and coke")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
